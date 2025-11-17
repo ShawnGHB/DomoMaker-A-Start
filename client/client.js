3c5a31dde60055b0a5b15d1917b346b67456f913
ShawnGHB <shawnshawn4116@gmail.com>
2025-11-17 14:46:45 -0500

    corrected mongo connection string

--- client/client.js
+++ client/client.js
@@ -8,7 +8,7 @@ const handleError = (message) => {
 };
 
 /* Sends post requests to the server using fetch. Will look for various
-   entries in the response JSON object, and will handle them appropriately.
+  entries in the response JSON object, and will handle them appropriately.
 */
 const sendPost = async (url, data) => {
   const response = await fetch(url, {
@@ -22,17 +22,17 @@ const sendPost = async (url, data) => {
   const result = await response.json();
   document.getElementById('domoMessage').classList.add('hidden');
 
-  if(result.redirect) {
+  if (result.redirect) {
     window.location = result.redirect;
   }
 
-  if(result.error) {
+  if (result.error) {
     handleError(result.error);
   }
 };
 
 /* Entry point of our client code. Runs when window.onload fires.
-   Sets up the event listeners for each form across the whole app.
+  Sets up the event listeners for each form across the whole app.
 */
 const init = () => {
   const signupForm = document.getElementById('signupForm');
@@ -41,11 +41,11 @@ const init = () => {
   const domoMessage = document.getElementById('domoMessage');
 
   /* If this page has the signupForm, add it's submit event listener.
-     Event listener will grab the username, password, and password2
-     from the form, validate everything is correct, and then will
-     use sendPost to send the data to the server.
+    Event listener will grab the username, password, and password2
+    from the form, validate everything is correct, and then will
+    use sendPost to send the data to the server.
   */
-  if(signupForm) {
+  if (signupForm) {
     signupForm.addEventListener('submit', (e) => {
       e.preventDefault();
       domoMessage.classList.add('hidden');
@@ -54,27 +54,27 @@ const init = () => {
       const pass = signupForm.querySelector('#pass').value;
       const pass2 = signupForm.querySelector('#pass2').value;
 
-      if(!username || !pass || !pass2) {
+      if (!username || !pass || !pass2) {
         handleError('All fields are required!');
         return false;
-      } 
+      }
 
-      if(pass !== pass2) {
+      if (pass !== pass2) {
         handleError('Passwords do not match!');
         return false;
       }
 
-      sendPost(signupForm.getAttribute('action'), {username, pass, pass2});
+      sendPost(signupForm.getAttribute('action'), { username, pass, pass2 });
       return false;
     });
   }
 
   /* If this page has the loginForm, add it's submit event listener.
-     Event listener will grab the username, password, from the form, 
-     validate both values have been entered, and will use sendPost 
-     to send the data to the server.
+    Event listener will grab the username, password, from the form, 
+    validate both values have been entered, and will use sendPost 
+    to send the data to the server.
   */
-  if(loginForm) {
+  if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
       e.preventDefault();
       domoMessage.classList.add('hidden');
@@ -82,22 +82,22 @@ const init = () => {
       const username = loginForm.querySelector('#user').value;
       const pass = loginForm.querySelector('#pass').value;
 
-      if(!username || !pass) {
+      if (!username || !pass) {
         handleError('Username or password is empty!');
         return false;
       }
 
-      sendPost(loginForm.getAttribute('action'), {username, pass});
+      sendPost(loginForm.getAttribute('action'), { username, pass });
       return false;
     });
   }
 
   /* If this page has the domoForm, add it's submit event listener.
-     Event listener will grab the domo name and the domo age from
-     the form. It will throw an error if one or both are missing.
-     Otherwise, it will send the request to the server.
+    Event listener will grab the domo name and the domo age from
+    the form. It will throw an error if one or both are missing.
+    Otherwise, it will send the request to the server.
   */
-  if(domoForm) {
+  if (domoForm) {
     domoForm.addEventListener('submit', (e) => {
       e.preventDefault();
       domoMessage.classList.add('hidden');
@@ -105,12 +105,12 @@ const init = () => {
       const name = domoForm.querySelector('#domoName').value;
       const age = domoForm.querySelector('#domoAge').value;
 
-      if(!name || !age) {
+      if (!name || !age) {
         handleError('All fields are required!');
         return false;
       }
 
-      sendPost(domoForm.getAttribute('action'), {name, age});
+      sendPost(domoForm.getAttribute('action'), { name, age });
       return false;
     });
   }
